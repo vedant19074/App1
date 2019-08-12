996a27943a31a82c38a2848780613990ab81060a
Hubot <vedantdesai13@gmail.com>
2019-08-12 13:03:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        Log.d("MYCODE", "It works");
+        Intent newIntent  = new Intent(this, SendActivity.class);
+        startActivity(newIntent);
+    }
+}
